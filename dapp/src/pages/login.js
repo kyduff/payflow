2f736be288a019c23d10e4662371fa96c098a4f1
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2023-07-21 18:59:05 +0200

    Conditionally render login with Monerium button

--- dapp/src/pages/login.js
+++ dapp/src/pages/login.js
@@ -1,11 +1,18 @@
-import { VStack } from "@chakra-ui/react";
+import { Button, VStack } from "@chakra-ui/react";
 import { Web3Button } from "@web3modal/react";
+import { useAccount } from "wagmi";
 
 export default function Login() {
+
+  const { address, isConnected } = useAccount();
+
   return (
     <VStack>
       <h1>payflow</h1>
       <Web3Button />
+      {isConnected && (
+        <Button>Login with Monerium</Button>
+      )}
     </VStack>
   )
 }
