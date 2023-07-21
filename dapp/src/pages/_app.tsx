57d4961a11cba093d10ca36416f560a03e02399c
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2023-07-21 18:36:34 +0200

    Add Chakra provider

--- dapp/src/pages/_app.tsx
+++ dapp/src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// pages/_app.js
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
+export default App;
