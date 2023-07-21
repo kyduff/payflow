c8af8d5c1bcd880dcc96efb20d43da050b907c7d
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2023-07-21 19:24:11 +0200

    Add all fields needed in monerium API call

--- dapp/src/components/RecipientDetails.tsx
+++ dapp/src/components/RecipientDetails.tsx
@@ -3,8 +3,10 @@ import { Input, VStack } from "@chakra-ui/react";
 export default function RecipientDetails() {
   return (
     <VStack>
-      <Input placeholder="Account"/>
+      <Input placeholder="Account holder"/>
       <Input placeholder="IBAN"/>
+      <Input placeholder="Amount"/>
+      <Input placeholder="Memo"/>
     </VStack>
   )
 }
