6484874fc072fbf04e420101dcd722e91665d125
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2023-07-22 16:49:17 +0200

    Replace safecore sdk auth with monerium sdk auth

--- dapp/src/pages/pay.tsx
+++ dapp/src/pages/pay.tsx
@@ -11,55 +11,48 @@ import { type WalletClient, getWalletClient } from '@wagmi/core'
 import { useNetwork } from "wagmi";
 import { useEffect, useState } from "react";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+//@ts-ignore
+import { MoneriumClient } from '@monerium/sdk';
+
+interface OrderDetails {
+  accountHolder: string,
+  iban: string,
+  amount: string,
+  memo: string,
+}
 
 export default function Pay({ code }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
-  const { chain } = useNetwork();
-
-  const CLIENT_ID = "efec9397-f584-11ed-8837-1e07284d4ad6";
-  // const SAFE_ADDRESS = "0xa208B9468730A17f7dd575d6762cAde9ebA6b1ec"; // Polygon
-  const SAFE_ADDRESS = "0x58da951b17Cb5A6449468f21e6887f81BBA73620"; // Gnosis
-  const SIGNER_ADDRESS = "0xb7CF83796d911eD42592a625B95753A3Cfdd7feE";
-  const REDIRECT_URL = 'http://localhost:3000/pay/';
-
   console.log(code);
+
+  const [client, setClient] = useState<MoneriumClient>();
+  const [orderDetails, setOrderDetails] = useState<OrderDetails>();
+
+  useEffect(() => {
+    ;(async () => {
+      if (!code) {
+        console.log("no code");
+        return;
+      }
+  
+      const _client = new MoneriumClient();
+  
+      await _client.auth({
+        client_id: "efec9397-f584-11ed-8837-1e07284d4ad6",
+        code: code,
+        code_verifier: window.localStorage.getItem("codeVerifier"),
+        redirect_uri: "http://localhost:3000/pay/"
+      })
+
+      setClient(_client);
+    })();
+  }, [code]);
 
   const handlePay = async function () {
 
-    // Configure Safe SDK
-    const provider = await getEthersSigner({ chainId: chain?.id });
-    if (!provider) return
-    const ethAdapter = new EthersAdapter({ ethers, signerOrProvider: provider.getSigner(SIGNER_ADDRESS) });
-    const safeSdk = await Safe.create({
-      ethAdapter: ethAdapter,
-      safeAddress: SAFE_ADDRESS,
-    });
-
-    // Configure Monerium
-    const moneriumPack = new MoneriumPack({
-      clientId: CLIENT_ID,
-      environment: 'sandbox',
-    })
-
-    await moneriumPack.init({ safeSdk });
-
-    const safeMoneriumClient = await moneriumPack.open({
-      authCode: code as string,
-      redirectUrl: REDIRECT_URL,
-    });
-    console.log(safeMoneriumClient);
-
-    const authContext = await safeMoneriumClient.getAuthContext()
-    const profile = await safeMoneriumClient.getProfile(authContext.defaultProfile)
-    const balances = await safeMoneriumClient.getBalances()
-    const orders = await safeMoneriumClient.getOrders()
-
-    console.log({
-      authContext,
-      profile,
-      balances,
-      orders,
-    })
+    // TODO: place order and subscribe to order status
+    const balances = await client.getBalances();
+    console.log(balances);
 
   }
 
@@ -68,7 +61,7 @@ export default function Pay({ code }: InferGetServerSidePropsType<typeof getServ
       <h1>Pay</h1>
       <ScanQR />
       <h2>or</h2>
-      <RecipientDetails />
+      <RecipientDetails setOrderDetails={setOrderDetails}/>
       <Button colorScheme="green" onClick={handlePay}>Pay</Button>
     </VStack>
   )
